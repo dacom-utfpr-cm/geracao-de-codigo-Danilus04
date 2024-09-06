fccc9dc5f2865aa4066d0f3755e76c6133f1ee1d
Danilus04 <danilogarciaba@hotmail.com>
2024-09-06 03:45:33 -0300

    le funções vazias

--- tppgencode.py
+++ tppgencode.py
@@ -20,10 +20,15 @@ from tpplex import tokens
 
 from mytree import MyNode
 from anytree.exporter import DotExporter, UniqueDotExporter
-from anytree import RenderTree, AsciiStyle
+from anytree import RenderTree, AsciiStyle, PreOrderIter
 
 from tppparser import generate_syntax_tree
+from tppsema import semanticMain
 from myerror import MyError
+
+#Biblioteca para geração de código
+from llvmlite import ir
+from llvmlite import binding as llvm
 
 error_handler = MyError('GenCodeErrors')
 
@@ -32,6 +37,89 @@ root = None
 showKey = False 
 haveTPP = False
 arrError = []
+
+#node, caminho = [0,1,0,0] (caminho que vai percorrer entre os filhos)
+#Retorna o node onde o caminho o levou
+def browseNode(node, caminho):
+    nodeAux = node
+    for prox in caminho:
+        if prox < 0: 
+            nodeAux = nodeAux.parent
+        else:
+            nodeAux = nodeAux.children[prox]
+    return nodeAux
+    
+#Ja que na propria arvore tem varios nomes para o mesmo tipo, essa função ve a entrada e diz corretamente o tipo
+def whatType(str):
+    inteiro = ["INTEIRO","inteiro","NUM_INTEIRO"]
+
+    if(str in inteiro):
+        return "INTEIRO"
+
+def createVar(str):
+
+    type = whatType(str)
+
+    if(type == "INTEIRO"):
+        return ir.IntType(32)
+
+def generateCode(tree):
+    llvm.initialize()
+    llvm.initialize_all_targets()
+    llvm.initialize_native_target()
+    llvm.initialize_native_asmprinter()
+
+    module = ir.Module('meu_modulo.bc')
+    module.triple = llvm.get_process_triple()
+    target = llvm.Target.from_triple(module.triple)
+    target_machine = target.create_target_machine()
+    module.data_layout = target_machine.target_data
+
+    print(module)
+
+
+    entryBlock = None
+    endBasicBlock = None
+    builder = None
+
+    for node in list(PreOrderIter(tree)):
+        nodeAux = None
+        type = None
+        var = None
+        functInfo = None
+        func = None
+        name = None
+
+        if(node.name == "declaracao_funcao"):
+            name = browseNode(node, [1,0,0]).name
+            type = browseNode(node, [0,0,0]).name
+            
+            var = createVar(type)
+            functInfo = ir.FunctionType(var, ())
+            func = ir.Function(module, functInfo, name=name)
+
+            entryBlock = func.append_basic_block('entry')
+            endBasicBlock = func.append_basic_block('exit')
+
+            builder = ir.IRBuilder(entryBlock)
+
+        if(node.name == "FIM"):
+            if(browseNode(node, [-1,-1]).name == "declaracao_funcao"):
+                # Cria um salto para o bloco de saída
+                builder.branch(endBasicBlock)
+                print('')
+                # Adiciona o bloco de saida
+                builder.position_at_end(endBasicBlock)
+
+
+
+
+            
+    arquive = open('./tests/meu_modulo.ll', 'w')
+    arquive.write(str(module))
+    arquive.close()
+
+        
 
 
 # Função Principal que recebe os argumentos
@@ -59,8 +147,11 @@ def main(args):
         else:
             
             tree = generate_syntax_tree(args)  
-
-            print(tree)      
+            
+            #semanticMain(args)
+            
+            generateCode(tree)
+                
             #if tree:
     
     except Exception as e:
